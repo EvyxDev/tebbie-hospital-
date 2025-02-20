c194a92a65ca59badfe6ccddb73dcdc4fb4c170b
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2025-02-20 13:15:42 +0200

    update on date

--- src/components/BookingDataDoctor.jsx
+++ src/components/BookingDataDoctor.jsx
@@ -1,15 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import SwitchDoctor from "./SwitchDoctor";
 import { attendanceDoctor } from "../utlis/https";
+import { MdBlock } from "react-icons/md";
 
 const BookingDataDoctor = ({ filteredBookings }) => {
   const doctorId = filteredBookings[0]?.doctor?.id;
   const date = filteredBookings[0]?.date;
   const status = filteredBookings[0]?.status;
 
-  const [attendanceStatus, setAttendanceStatus] = useState(status === "cancelled" ? false : true);
-console.log(filteredBookings)
+  const [attendanceStatus, setAttendanceStatus] = useState();
+  console.log(filteredBookings);
+
   const queryClient = useQueryClient();
   const token = localStorage.getItem("authToken");
 
@@ -18,11 +20,11 @@ console.log(filteredBookings)
     onSuccess: () => {
       queryClient.invalidateQueries("doctor-attendance");
       alert("تم تأكيد غياب الدكتور بنجاح");
-      setAttendanceStatus("")
+      setAttendanceStatus("");
     },
     onError: (error) => {
       console.error("Error assigning specialization:", error);
-      setAttendanceStatus("")
+      setAttendanceStatus("");
 
       alert("حدث خطأ أثناء تأكيد غياب الدكتور");
     },
@@ -30,7 +32,7 @@ console.log(filteredBookings)
 
   const handleAttendanceToggle = (checked) => {
     if (status === "pending") {
-      setAttendanceStatus(checked); 
+      setAttendanceStatus(checked);
       mutation.mutate({
         token: token,
         doctor_id: doctorId,
@@ -41,38 +43,51 @@ console.log(filteredBookings)
     }
   };
 
+  const isFutureDate = (bookingDate) => {
+    const now = new Date();
+    const bookingDateTime = new Date(bookingDate);
+    return bookingDateTime > now;
+  };
+
   return (
     <>
-      {filteredBookings.length > 0 ? (
-        status === "pending" && (
-          
-          <div className="flex gap-1 justify-between items-end">
-            <h2 className="font-medium">
-              حجوزات اليوم
-            </h2>
-            <div className="flex gap-1 justify-end items-end">
-            <p className="text-[#8F9BB3] text-md">تأكيد حضور</p>
-            <SwitchDoctor
-              checked={attendanceStatus}
-              onChange={handleAttendanceToggle}
-            />
+      <div className="flex gap-1 justify-between items-end">
+        <h2 className="font-medium">حجوزات اليوم</h2>
+        <div className="flex gap-1 justify-end items-end">
+          {isFutureDate(date) ? (
+            <>
+              <p className="text-[#8F9BB3] text-md">تأكيد حضور</p>
+              <SwitchDoctor
+                checked={attendanceStatus}
+                onChange={handleAttendanceToggle}
+              />
+            </>
+          ) : (
+            <div className="text-red-500 flex justify-center items-center gap-2">
+              <p>الموعد قد مرّ</p>
+              {/* <MdBlock size={25} /> */}
             </div>
-          </div>
-        )
-      ) : (
-        <p className="text-center text-gray-600 font-medium">لا توجد حجوزات للتاريخ المختار</p>
-      )}
+          )}
+        </div>
+      </div>
 
       {filteredBookings.map((booking) => (
-        <div key={booking.id} className="my-4 shadow-sm bg-white rounded-lg py-2 ">
-          <div className=" p-4 rounded-lg">
+        <div
+          key={booking.id}
+          className="my-4 shadow-sm bg-white rounded-lg py-2 "
+        >
+          <div className="p-4 rounded-lg">
             <span className="flex gap-2">
               <div className="InputPrimary mt-1" />
               <p className="font-medium">{booking.date}</p>
             </span>
-            <h3 className="text-xl font-normal my-1">{booking.user?.name || "اسم غير متوفر"}</h3>
+            <h3 className="text-xl font-normal my-1">
+              {booking.user?.name || "اسم غير متوفر"}
+            </h3>
             <div className="flex justify-between">
-              <h3 className="text-[#8F9BB3] text-md">{booking.doctor?.name || "غير متوفر"}</h3>
+              <h3 className="text-[#8F9BB3] text-md">
+                {booking.doctor?.name || "غير متوفر"}
+              </h3>
             </div>
           </div>
         </div>
