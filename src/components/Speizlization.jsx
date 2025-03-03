4d521f7ffb7e91bec2a24ad2be7a2920c56d05b0
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2025-03-03 14:48:18 +0200

    new update

--- src/components/Speizlization.jsx
+++ src/components/Speizlization.jsx
@@ -13,36 +13,46 @@ const Speizlization = () => {
     data: specializationData,
     error,
     isLoading,
-} = useQuery({
+  } = useQuery({
     queryKey: ["specialization", speizId],
     queryFn: () => getSpecialization({ id: speizId, token }),
     enabled: !!speizId,
-});
+  });
 
   if (isLoading) return <LoaderComponent />;
   if (error)
     return (
       <div className="h-screen w-full flex justify-center items-center text-md text-red-400">
-      <p className="text-red-400">عذرا حدث خطأ ما</p>
+        <p className="text-red-400">عذرا حدث خطأ ما</p>
       </div>
     );
+
+  if (!specializationData ) {
+    return (
+      <div className="h-screen w-full flex justify-center items-center text-lg ">
+        <p >  لا توجد حجوزات لهذا الطبيب في هذا التاريخ  </p>
+      </div>
+    );
+  }
 
   return (
     <section className="w-full h-full">
       <div className="flex flex-col gap-6">
         <div className="bg-[#EDF0FA] h-36 rounded-md mt-8 relative">
           <div className="absolute bottom-0 left-8 h-28">
-            <img
-              className="h-28 w-28 object-cover"
-              alt="doctor Image"
-              src={specializationData.specialization.firstMediaUrl}
-              loading="lazy"
-            />
+            {specializationData.specialization.firstMediaUrl && (
+              <img
+                className="h-28 w-28 object-cover"
+                alt="doctor Image"
+                src={specializationData.specialization.firstMediaUrl}
+                loading="lazy"
+              />
+            )}
           </div>
         </div>
 
         <div className="flex gap-3">
-        <Link
+          <Link
             to="/doctors"
             className="bg-[#EDF0FA] h-36 rounded-md w-full p-4 flex flex-col justify-between"
           >
@@ -69,32 +79,37 @@ const Speizlization = () => {
             </div>
             <h2 className="text-[#677294] font-[500]">الاستردادات</h2>
           </Link>
-        
         </div>
 
         <div className="w-full h-full">
           <h2 className="font-[500] text-lg mb-8">الحجز القادم</h2>
-          {specializationData.bookings.map((booking) => (
-            <div
-              key={booking.id}
-              className="my-4 shadow-sm bg-white rounded-lg py-2 p-4"
-            >
-              <span className="flex gap-2">
-                <div className="InputPrimary mt-1" />
-                <p className="font-medium">{booking.time}</p>
-              </span>
-              <h3 className="text-xl font-medium my-1">{booking.user.name}</h3>
-              <div className="flex justify-between">
-                <h3 className="text-[#8F9BB3] text-md">
-                  {booking.doctor.name}
-                </h3>
-                <div className="flex gap-1 justify-center items-center">
-                  <p className="text-[#8F9BB3] text-md">تأكيد حضور</p>
-                  <Switch checked={booking.status === "finished"} />
+          {specializationData.bookings && specializationData.bookings.length > 0 ? (
+            specializationData.bookings.map((booking) => (
+              <div
+                key={booking.id}
+                className="my-4 shadow-sm bg-white rounded-lg py-2 p-4"
+              >
+                <span className="flex gap-2">
+                  <div className="InputPrimary mt-1" />
+                  <p className="font-medium">{booking.time}</p>
+                </span>
+                <h3 className="text-xl font-medium my-1">{booking.user.name}</h3>
+                <div className="flex justify-between">
+                  <h3 className="text-[#8F9BB3] text-md">
+                    {booking.doctor.name}
+                  </h3>
+                  <div className="flex gap-1 justify-center items-center">
+                    <p className="text-[#8F9BB3] text-md">تأكيد حضور</p>
+                    <Switch checked={booking.status === "finished"} />
+                  </div>
                 </div>
               </div>
+            ))
+          ) : (
+            <div className="text-center text-gray-500 p-4 bg-white rounded-md shadow-sm">
+              <p>لا توجد حجوزات حالياً</p>
             </div>
-          ))}
+          )}
         </div>
       </div>
     </section>
